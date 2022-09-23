625654f542f67ce3bd5510d3c7635c265ed54c5a
Chrisdocs <chriswolfe.dev@gmail.com>
2022-09-23 18:21:42 -0500

    material ui login/ signup templates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import Button from '@mui/material/Button';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-			<h2>App</h2>
-			<Button variant="contained">Hello World</Button>
+			<Login />
     </div>
   );
 }

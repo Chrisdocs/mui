e55150139b1f69296645d5e2d6cf64154daa511c
Chrisdocs <chriswolfe.dev@gmail.com>
2022-09-23 18:06:45 -0500

    installed material ui and dependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
 			<h2>App</h2>
+			<Button variant="contained">Hello World</Button>
     </div>
   );
 }
